2c4b0169ac6a59c1a8ffb1dae535de53203738fb
Mario Paone <paonemario@yahoo.it>
2025-09-25 14:41:02 +0200

    fix wakeUp api

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { register, login } from "../controllers/auth.controller";
+import { register, login, wakeUp } from "../controllers/auth.controller";
 
 const authRouter = Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
+authRouter.get("/wakeUp", wakeUp);
 
 export default authRouter;
